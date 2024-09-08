df953cdc3ec73efacad2754972de93ccf483a12b
Leandro <leandroviscolungo@gmail.com>
2024-09-08 16:56:12 -0300

    small tile change atialiased text?

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,7 +3,7 @@
 export default function Skills() {
 
     return (
-        <div id="skills" className="h-screen bg-[#46558b]">
+        <div id="skills" className="h-screen bg-[#082388]">
           <h1>Habilidades</h1>
         </div>
     )
