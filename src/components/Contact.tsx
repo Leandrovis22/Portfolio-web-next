d3947416aa4fae456caf8e3c002aeeb15c6f0ce1
Leandro <leandroviscolungo@gmail.com>
2024-09-19 14:02:45 -0300

    textarea fixed

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -75,7 +75,7 @@ export default function Contact() {
     };
 
     return (
-        <div id="contact" className="h-screen-footer relative overflow-x-clip flex flex-col flex-grow">
+        <div id="contact" className=" lg:h-screen-footer relative overflow-x-clip flex flex-col flex-grow">
 
             <div className="absolute h-[400px] w-[1600px] bottom-[-4rem] left-1/2 -translate-x-1/2 dark:bg-secondary [mask-image:radial-gradient(64%_78%_at_bottom_center,black,transparent)] z-0"></div>
 
@@ -83,7 +83,7 @@ export default function Contact() {
                 Contacto
             </h6>
 
-            <div className="flex h-full z-10 px-[5%] 2xl-1800:mx-[10%]">
+            <div className="md:flex-row flex-col flex h-fit lg:gap-0 gap-5 lg:h-full z-10 px-4 lg:px-[5%] 2xl-1800:mx-[10%]">
 
                 {/* Imagen */}
                 <div className="flex-1 flex flex-col h-full justify-center items-center">
@@ -97,7 +97,7 @@ export default function Contact() {
                 </div>
 
                 {/* Botones de contacto */}
-                <div className="flex-1 px-4 items-center flex justify-center">
+                <div className="flex-1 px-4 items-center justify-center hidden lg:flex">
                     <div className="flex items-center flex-col gap-5 w-fit">
                         <div className="flex gap-4">
                             <Button variant="ghost" className="size-[5rem] flex items-center gap-2 w-fit min-h-[60px]" radius="full" aria-label="Linkedin">
@@ -125,11 +125,13 @@ export default function Contact() {
 
                 {/* Formulario de contacto */}
                 <div className="flex-1 flex flex-col h-full justify-center items-center">
-                    <div className="w-full max-w-[455.6px] mx-auto">
-                        <div className="relative w-full pb-[108%]"> {/* 491.6 / 455.6 ≈ 108% */}
-                            <div className="absolute inset-0 card rounded-3xl shadow-lg content-center p-4 overflow-y-auto">
-                                <h2 className="text-2xl font-bold mb-[5%] text-center">Enviar un Mensaje</h2>
-                                <form onSubmit={handleSubmit} className="space-y-4">
+                    <div className="w-full max-w-[455.6px] mx-auto h-full">
+                        <div className="relative w-full pb-[108%] h-full"> {/* 491.6 / 455.6 ≈ 108% */}
+                            <div className="absolute inset-0 card rounded-3xl shadow-lg p-4 flex flex-col">
+                                <h2 className="text-2xl font-bold mb-4 text-center">Enviar un Mensaje</h2>
+
+                                {/* Contenedor flexible para inputs y textarea */}
+                                <div className="flex-grow flex flex-col space-y-4 overflow-auto min-h-0">
                                     <div>
                                         <Input
                                             type="text"
@@ -150,16 +152,21 @@ export default function Contact() {
                                         />
                                         {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email}</p>}
                                     </div>
-                                    <div>
+                                    <div className="flex-1 h-full">
                                         <Textarea
                                             placeholder="Mensaje"
                                             value={mensaje}
                                             onChange={(e) => setMensaje(e.target.value)}
                                             required
-                                            className="lg-1080:h-auto h-[2.5rem]"
+                                            disableAutosize
+                                            className="h-full"
+                                            classNames={{
+                                                inputWrapper: "!h-full",  // Aplica la altura directamente al input-wrapper
+                                            }}
                                         />
                                         {errors.mensaje && <p className="text-red-500 text-sm mt-1">{errors.mensaje}</p>}
                                     </div>
+                                   
                                     <Input
                                         type="text"
                                         className="hidden"
@@ -167,20 +174,57 @@ export default function Contact() {
                                         onChange={(e) => setBotField(e.target.value)}
                                         placeholder="Dejar vacío"
                                     />
-                                    <div className="flex justify-center">
-                                        <Button type="submit" color="primary"
-                                            variant="shadow"
-                                            size="lg"
-                                            className="uppercase flex items-center gap-2 w-1/2">
-                                            Enviar
-                                        </Button>
-                                    </div>
-                                </form>
+                                </div>
+
+                                {/* Botón al final */}
+                                <div className="mt-4 flex justify-center">
+                                    <Button
+                                        type="submit"
+                                        color="primary"
+                                        variant="shadow"
+                                        size="lg"
+                                        className="uppercase flex items-center gap-2 w-1/2"
+                                    >
+                                        Enviar
+                                    </Button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
 
+
+            </div>
+
+
+
+            <div className="h-full content-center">
+                {/* Botones de contacto para pantalla menor a lg */}
+                <div className=" px-4 flex lg:hidden">
+                    <div className="flex justify-center flex-wrap flex-row gap-5 w-fit">
+                        <div className="flex gap-4">
+                            <Button variant="ghost" className="size-[5rem] flex items-center gap-2 w-fit min-h-[60px]" radius="full" aria-label="Linkedin">
+                                <div className="size-[2.6rem] items-center content-center justify-center flex">
+                                    <FiDownload className="size-[2.2rem]" /></div> <p className="text-lg">Descargar CV</p>
+                            </Button>
+                        </div>
+                        <Button
+                            variant="ghost"
+                            className="size-[5rem] flex items-center gap-2 w-fit min-w-[60px] min-h-[60px]"
+                            radius="full"
+                            aria-label="Correo"
+                            onClick={copyToClipboard}
+                        >
+                            <div className="size-[2.6rem] items-center content-center justify-center flex"><IoIosMail className="size-[2.4rem]" /></div> <p className="text-lg">{buttonText}</p>
+                        </Button>
+                        <Button variant="ghost" className="size-[5rem] flex items-center gap-2 w-fit min-w-[60px] min-h-[60px]" radius="full" aria-label="Linkedin">
+                            <div className="size-[2.6rem] items-center content-center justify-center flex"><BsLinkedin className="size-[1.9rem]" /> </div> <p className="text-lg">leandroviscolungo</p>
+                        </Button>
+                        <Button variant="ghost" className="size-[5rem] flex items-center gap-2 w-fit min-w-[60px] min-h-[60px]" radius="full" aria-label="Github">
+                            <div className="size-[2.6rem] items-center content-center justify-center flex"><BsGithub className="size-[2.2rem]" /> </div> <p className="text-lg">Leandrovis22</p>
+                        </Button>
+                    </div>
+                </div>
             </div>
         </div>
     );
