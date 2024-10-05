b0be38ebc38301d991063d96558ed9308468fd68
Leandro <leandroviscolungo@gmail.com>
2024-10-05 02:32:29 -0300

    just BASE_URL

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -52,7 +52,7 @@ export async function getData(): Promise<{
   footer: FooterData;
 }> {
   try {
-    const URL = process.env.NEXT_PUBLIC_BASE_URL;
+    const URL = process.env.BASE_URL;
 
     console.log('Fetching data from API...');
     const response = await fetch(`${URL}/api/data`, {
